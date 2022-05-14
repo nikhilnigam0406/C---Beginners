b1fb436b32403bf45d5bdb148d8c2bf07abfa32b
Nikhil <nikhilnigam2001@gmail.com>
2022-05-14 15:58:30 +0530

    Date - 13_may_2022

---
+++ Basic/basicProgram.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // cout<<"This is c++ code";
+    // Pointers
+    // int a = 4;
+    // int *b = &a;
+    // cout << &b<<endl;
+    // cout << &a;
+// array
+    // int arr[] ={ 45,57,85,40,36,-44};
+    // for(int i=0; i<5; i++){
+    //     cout<< " Print array " <<arr[i]<<endl;
+
+    // }
+
+// Structure
+
+struct student{
+        int roll_no;
+        int age;
+        int 
+
+}
+
+
+
+
+
+
+
+
+
+}
